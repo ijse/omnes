f43081bd1d5e550e91289a203fdd312522554d27
ijse <i@ijser.cn>
2012-09-19 15:11:17 +0800

    add nodejs modules

--- public/app.js
+++ public/app.js
@@ -1,10 +1,16 @@
+
 Ext.application({
-    name: 'HelloExt',
+    name: 'Omnes',
+
+    appFolder: 'app',
+    controllers: ["Users"],
+
     launch: function() {
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
             items: [
                 {
+                    xtype: 'userList',
                     title: 'Hello Ext',
                     html : 'Hello! Welcome to Ext JS.'
                 }
